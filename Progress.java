982f1ac838d1ded9818b48a09284f7e393ba22b8
karennassef4 <nassefkaren5@gmail.com>
2025-11-18 15:55:13 +0200

    Refactor Progress class to use JSONObject for data

--- Progress.java
+++ Progress.java
@@ -2,28 +2,125 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package models;
+package lab7.isa;
 
+import org.json.*;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Progress {
-    private String courseId;
-    private Set<String> completedLessonIds = new HashSet<>();
-
-    public Progress() {}
-
-    public Progress(String courseId) {
-        this.courseId = courseId;
+    private JSONObject progressData;
+    
+    public Progress(JSONObject progressData) {
+        this.progressData = progressData;
+        if (!progressData.has("completedLessonIds")) {
+            progressData.put("completedLessonIds", new JSONArray());
+        }
     }
+    
+    public Progress(int courseId) {
+        this.progressData = new JSONObject();
+        progressData.put("courseId", courseId);
+        progressData.put("completedLessonIds", new JSONArray());
+    }
+    
+    public Progress() {
+        this.progressData = new JSONObject();
+        progressData.put("completedLessonIds", new JSONArray());
+    }
+    
 
-    public String getCourseId() { return courseId; }
-    public void setCourseId(String courseId) { this.courseId = courseId; }
+    public JSONObject toJSON() {
+        return progressData;
+    }
+    
 
-    public Set<String> getCompletedLessonIds() { return completedLessonIds; }
-    public void setCompletedLessonIds(Set<String> completedLessonIds) { this.completedLessonIds = completedLessonIds; }
+    public int getCourseId() {
+        return progressData.optInt("courseId", -1);
+    }
+    
 
-    public void markLessonCompleted(String lessonId) { completedLessonIds.add(lessonId); }
-    public boolean isLessonCompleted(String lessonId) { return completedLessonIds.contains(lessonId); }
+    public void setCourseId(int courseId) {
+        progressData.put("courseId", courseId);
+    }
+    
+
+    public Set<Integer> getCompletedLessonIds() {
+        Set<Integer> lessonIds = new HashSet<>();
+        JSONArray lessonsArray = progressData.optJSONArray("completedLessonIds");
+        if (lessonsArray != null) {
+            for (Object obj : lessonsArray) {
+                if (obj instanceof Integer) {
+                    lessonIds.add((Integer) obj);
+                } else if (obj instanceof String) {
+                    try {
+                        lessonIds.add(Integer.parseInt((String) obj));
+                    } catch (NumberFormatException e) {
+                    }
+                }
+            }
+        }
+        return lessonIds;
+    }
+    
+    public void setCompletedLessonIds(Set<Integer> completedLessonIds) {
+        JSONArray lessonsArray = new JSONArray();
+        for (Integer lessonId : completedLessonIds) {
+            lessonsArray.put(lessonId);
+        }
+        progressData.put("completedLessonIds", lessonsArray);
+    }
+    
+
+    public void markLessonCompleted(int lessonId) {
+        JSONArray lessonsArray = progressData.optJSONArray("completedLessonIds");
+        if (lessonsArray == null) {
+            lessonsArray = new JSONArray();
+            progressData.put("completedLessonIds", lessonsArray);
+        }
+        
+        for (Object obj : lessonsArray) {
+            if (obj instanceof Integer && (Integer) obj == lessonId) {
+                return; 
+            }
+        }
+        
+        lessonsArray.put(lessonId);
+    }
+    
+    public boolean isLessonCompleted(int lessonId) {
+        JSONArray lessonsArray = progressData.optJSONArray("completedLessonIds");
+        if (lessonsArray == null) return false;
+        
+        for (Object obj : lessonsArray) {
+            if (obj instanceof Integer && (Integer) obj == lessonId) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void unmarkLessonCompleted(int lessonId) {
+        JSONArray lessonsArray = progressData.optJSONArray("completedLessonIds");
+        if (lessonsArray == null) return;
+        
+        JSONArray newArray = new JSONArray();
+        for (Object obj : lessonsArray) {
+            if (!(obj instanceof Integer && (Integer) obj == lessonId)) {
+                newArray.put(obj);
+            }
+        }
+        progressData.put("completedLessonIds", newArray);
+    }
+    
+    public double getCompletionPercentage(int totalLessons) {
+        if (totalLessons == 0) return 0.0;
+        return (getCompletedLessonIds().size() * 100.0) / totalLessons;
+    }
+    
+    @Override
+    public String toString() {
+        return "Progress for Course " + getCourseId() + 
+               " - Completed: " + getCompletedLessonIds().size() + " lessons";
+    }
 }
-
